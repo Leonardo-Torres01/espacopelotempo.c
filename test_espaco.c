b1b0fbf9ca4833143d50f3252a565c2b1620de2d
Leonardo <leomatorres1901@gmail.com>
2026-05-13 20:42:21 -0300

    Create test_espaco.c

---
+++ test_espaco.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_espaco.c"
+
+int main() 
+   {
+    int erros = 0;
+
+   
+    if (espaco(80, 2, 0, 0) != 160) erros++;
+
+   
+    if (espaco(40, 2, 0, 0) != 80) erros++;
+
+    
+    if (espaco(40, 2, 3, 1) != 86) erros++;
+
+    if (erros == 0) 
+    {
+        printf("Todos os testes de Espaco: PASSOU\n");
+    } 
+    else 
+    {
+        printf("Erro nos testes: %d falha(s)\n", erros);
+    }
+
+    printf("Criado por Leonardo Torres");
+
+    return 0;
+}
