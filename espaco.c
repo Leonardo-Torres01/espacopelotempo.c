68aa607bddda43cc92f30b0aab8e6e3be472a5f4
Leonardo <leomatorres1901@gmail.com>
2026-05-13 20:41:10 -0300

    Create espaco.c

---
+++ espaco.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "function_espaco.c"
+
+int main() 
+{
+    float v, t, a = 0;
+    int opcao;
+
+    printf("--- Calculo de Espaco Percorrido ---\n");
+    printf("Digite a velocidade (v): ");
+    scanf("%f", &v);
+    printf("Digite o tempo (t): ");
+    scanf("%f", &t);
+
+    printf("Ha aceleracao? (1 - Sim / 0 - Nao): ");
+    scanf("%d", &opcao);
+
+    if (opcao == 1) 
+    {
+        printf("Digite o valor da aceleracao (a): ");
+        scanf("%f", &a);
+    }
+
+    float resultado = espaco(v, t, a, opcao);
+    printf("\nO espaco percorrido e: %.2f\n", resultado);
+
+  
+    printf("Criado por Leonardo Torres");
+
+    return 0;
+}
