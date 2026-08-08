08518f631546033bc14e099661c4e23c9261d76e
Leonardo <leomatorres1901@gmail.com>
2026-08-08 12:41:00 -0300

    Update test_espacopelotempo.c

--- test_espacopelotempo.c
+++ test_espacopelotempo.c
@@ -1,29 +1,11 @@
-#include <stdio.h>
 #include "function_espacopelotempo.c"
+#include <stdio.h>
+#include <assert.h>
 
-int main() 
-   {
-    int erros = 0;
+int main(int argc, char const *argv[])
+{
+	assert(espacopelotempo(80, 2, 0) == 160 && espacopelotempo(40, 2, 0) == 80 && espacopelotempo(40, 2, 3) == 86);
+	printf("Teste concluido com sucesso");
 
-   
-    if (espaco(80, 2, 0, 0) != 160) erros++;
-
-   
-    if (espaco(40, 2, 0, 0) != 80) erros++;
-
-    
-    if (espaco(40, 2, 3, 1) != 86) erros++;
-
-    if (erros == 0) 
-    {
-        printf("Todos os testes de Espaco: PASSOU\n");
-    } 
-    else 
-    {
-        printf("Erro nos testes: %d falha(s)\n", erros);
-    }
-
-    printf("Criado por Leonardo Torres");
-
-    return 0;
+	return 0;
 }
